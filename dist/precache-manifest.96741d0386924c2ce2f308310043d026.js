00d9d6c24ccb761fb19d5361e554ed9ca8aed4f2
duoblogger <ahnafshahriar92@gmail.com>
2021-09-15 00:25:18 +0600

    added some stuff

---
+++ dist/precache-manifest.96741d0386924c2ce2f308310043d026.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "457190021c7de9380b2e",
+    "url": "/css/app.c8076335.css"
+  },
+  {
+    "revision": "7441302731aaf1b9da39",
+    "url": "/css/chunk-vendors.62a1d7e8.css"
+  },
+  {
+    "revision": "ab0662a146bdb27e644f0556e1c76cda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "457190021c7de9380b2e",
+    "url": "/js/app.2e5b9cdb.js"
+  },
+  {
+    "revision": "7441302731aaf1b9da39",
+    "url": "/js/chunk-vendors.e0e3250b.js"
+  },
+  {
+    "revision": "833d8182528a24c31b14f9eb0af10af2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
